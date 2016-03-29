ff4baf7d98ea1980dd2a2fe73fb211496e6c8ec0
icepy <xiangwenwe@foxmail.com>
2016-03-29 17:30:43 +0800

    init tools

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var url = require('./util/url');
+var store = require('./store/locationStore');
+var cookie = require('./store/cookie');
+
+module.exports = {
+    url:url,
+    store:store,
+    cookie:cookie
+}
